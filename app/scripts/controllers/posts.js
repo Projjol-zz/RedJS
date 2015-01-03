3e06aa7c9900d6bba97adf2f6d58adca8853c320
Projjol Banerji <probaner23@gmail.com>
2015-01-03 23:11:40 +0530

    posts with add and del

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,5 +1,14 @@
 'use strict';
 
 app.controller('PostCtrl', function($scope) {
-
+	$scope.posts = [];
+	$scope.post = {url: 'http://', title: ''};
+	
+	$scope.submitPost = function(){
+		$scope.posts.push($scope.post);
+		$scope.post = {url: 'http://', title: ''};
+	};
+	$scope.deletePost = function(index){
+		$scope.posts.splice(index,1);
+	};
 });
